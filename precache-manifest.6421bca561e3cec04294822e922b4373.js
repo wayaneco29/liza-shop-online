829bf91b313a264bab6fbe18883a110eb8ffc108
Wayan Danyael Eco <wayaneco29@gmail.com>
2019-08-07 23:04:58 +0800

    Updates

---
+++ precache-manifest.6421bca561e3cec04294822e922b4373.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "859bf1be3ccadd76846576b2c3448510",
+    "url": "/liza-shop-online/index.html"
+  },
+  {
+    "revision": "e2ac7d4c488c7ae55b3d",
+    "url": "/liza-shop-online/static/css/main.d980463a.chunk.css"
+  },
+  {
+    "revision": "6f2fc4abc3d60170f18d",
+    "url": "/liza-shop-online/static/js/2.d1f68066.chunk.js"
+  },
+  {
+    "revision": "e2ac7d4c488c7ae55b3d",
+    "url": "/liza-shop-online/static/js/main.5686c625.chunk.js"
+  },
+  {
+    "revision": "539eb859dc8769df656b",
+    "url": "/liza-shop-online/static/js/runtime~main.3bd2ec69.js"
+  }
+]);
